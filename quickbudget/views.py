2ad685ac2300019aa00c58b45a7c0669373e8e9f
Mateusz Liwinski <Mateusz.Liwinski@dataart.com>
2023-04-13 20:18:57 +0200

    Added permissions

--- quickbudget/views.py
+++ quickbudget/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 
+from quickbudget.api.permissions import BudgetMembersOnly, \
+    AllBudgetExpenseMembersOnly, BudgetExpenseMembersOnly
 from quickbudget.api.serializers import ExpenseSerializer, \
     CategoriesSerializer, BudgetSerializer
 from quickbudget.models import Budget, Expense, Category
@@ -17,11 +19,13 @@ class BudgetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
     lookup_url_kwarg = "budget_id"
+    permission_classes = [AllBudgetExpenseMembersOnly]
 
 
 class AddExpenses(generics.CreateAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    permission_classes = [BudgetExpenseMembersOnly]
 
 
 class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView):
