2183f707c92c2d7352ee075719910e3beb4ddbee
Mateusz Liwinski <Mateusz.Liwinski@dataart.com>
2023-05-06 21:23:14 +0200

    feat: set default ordering to "last_interaction"

--- quickbudget/views.py
+++ quickbudget/views.py
@@ -1,7 +1,7 @@
-from django.http import Http404
 from rest_framework import generics, status, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from quickbudget.api.permissions import (
     BudgetMembersOnly,
@@ -22,6 +22,7 @@ from quickbudget.models import Budget, Expense, Category, User
 
 class ListAddBudgets(generics.ListCreateAPIView):
     serializer_class = BudgetSerializer
+    ordering = ["-last_interaction"]
 
     def get_queryset(self):
         return Budget.objects.filter(members=self.request.user)
@@ -44,12 +45,10 @@ class ListAddBudgets(generics.ListCreateAPIView):
 
 
 class BudgetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
     lookup_url_kwarg = "budget_id"
     permission_classes = [IsBudgetOwnerOrSafeMethods]
-
-    def get_queryset(self):
-        return Budget.objects.filter(members=self.request.user)
 
 
 class AddExpenses(generics.CreateAPIView):
@@ -62,16 +61,15 @@ class ListAddExpenses(generics.ListCreateAPIView):
     serializer_class = ExpenseSerializer
     permission_classes = [AllBudgetExpenseMembersOnly]
     filter_backends = (filters.OrderingFilter,)
-    ordering_fields = ["name", "total", "created_timestamp"]
-    ordering = ["created_timestamp"]
+    ordering_fields = ["name", "total", "created_timestamp", "category"]
+    ordering = ["-created_timestamp"]
 
     def perform_create(self, serializer):
         # Set the creator field to the current authenticated user
         serializer.validated_data['author'] = self.request.user
 
         if not serializer.is_valid():
-            return Response(serializer.data,
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
         # Create the budget object
         budget = serializer.save()
@@ -79,12 +77,7 @@ class ListAddExpenses(generics.ListCreateAPIView):
 
     def get_queryset(self):
         budget = self.kwargs["budget_id"]
-        queryset = Expense.objects.filter(budget_id=budget)\
-        #     .order_by(
-        #     "created_timestamp")
-        # category = self.request.query_params.get("category")
-        # if category is not None:
-        #     queryset = queryset.filter(category=category)
+        queryset = Expense.objects.filter(budget_id=budget)
 
         return queryset
 
@@ -108,37 +101,27 @@ class BudgetMembers(APIView):
     def get_queryset(self):
         return Budget.objects.filter(creator=self.request.user)
 
-    def get_object(self, budget_id):
-        try:
-            return Budget.objects.get(id=budget_id)
-        except Budget.DoesNotExist:
-            raise Http404
-
     def put(self, request, budget_id, format=None):
-        budget = self.get_object(budget_id)
+        budget = get_object_or_404(Budget.objects, id=budget_id)
         serializer = MemberAddSerializer(budget, data=request.data)
-        self.check_object_permissions(request, budget)
 
         if self.request.user.id != budget.creator_id:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
         if not serializer.is_valid():
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         serializer.save()
         return Response(serializer.data)
 
     def delete(self, request, budget_id, format=None):
         budget = self.get_object(budget_id)
         serializer = RemoveBudgetMembersSerializer(budget, data=request.data)
-        self.check_object_permissions(request, budget)
 
         """1. Creator can only remove other users (non-creator member)
            2. Creator can remove himself
            3. Creator should be able to access without being a member"""
 
-        deleted_member = int(self.request.data['members'][0])
-        if deleted_member == budget.creator_id:
+        if budget.creator_id in self.request.data['members']:
             return Response(status=status.HTTP_403_FORBIDDEN)  # prevent creator from removing himself from the member table
 
         if not serializer.is_valid():
@@ -158,8 +141,7 @@ class QuickbudgetUsers(APIView):
         serializer = UserSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
